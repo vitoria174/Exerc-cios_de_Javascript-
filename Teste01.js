61577d11e57a638527e942556aa67b1dc6edd09c
vitoria174 <72430735+vitoria174@users.noreply.github.com>
2023-06-13 06:32:13 -0400

    Create Teste01.js

---
+++ Teste01.js
@@ -0,0 +1,11 @@
+var nome=window.prompt("Digite seu nome:")
+
+var num=Number(window.prompt("Digite um mumero:"))
+
+console.log(`Ola ${nome}, Tudo bem!`)
+
+console.log(`Seu nome Maiusculo ${nome.toUpperCase()}`)
+
+console.log(`Seu nome minúsculo ${nome.toLowerCase()}`)
+
+console.log(`Numero digitado ${num}`)
